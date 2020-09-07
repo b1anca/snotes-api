29294fcb8ec1cbb2908e500b90cc1fc9e654ca8b
bianca vieira <bncvr@outlook.com>
2020-09-06 23:44:10 -0300

    Fix cloudinary setup

--- app/serializers/note_serializer.rb
+++ app/serializers/note_serializer.rb
@@ -1,10 +1,8 @@
 class NoteSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
-  attributes :id, :name, :description, :image, :created_at
+  attributes :id, :name, :description, :image_url, :created_at
 
-  def image
-    if object.image.attached?
-      { url: rails_blob_url(object.image) }
-    end
+  def image_url
+    rails_blob_url(object.image) if object.image.attached?
   end
 end
